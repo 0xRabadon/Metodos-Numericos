f7ff5b3d64ded6a5bd13f46983f8e55736ac48db
0xRabadon <lovallenunez@gmail.com>
2026-03-19 12:26:01 -0300

    30 min lesenado con numpy y vent ayudaaaa

---
+++ biseccion.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+#DEBO INGRESAR PARAMETROS
+def biseccion(f, a, b, tol, mex_iter):
+    # Paso 1
+    i=1
+    return
+
+
+
+def funcion(x):
+    return x**2 - 2 # ejemplo de funcion: f(x) = x*2 -2
+
+a = 0 #limite inferior del intervalo
+b = 3 # Limite superior del intervalo
+tol = 0.0000001
+
